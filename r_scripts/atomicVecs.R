c52d843efdc4d4a69ebd060b703f6898b01ea7a8
Gwen Phillips <gwendolyn.phillips@uvm.edu>
2026-01-22 13:31:53 -0500

    R_file

---
+++ r_scripts/atomicVecs.R
@@ -0,0 +1,9 @@
+### This is a document describing vectors in R
+### 22-Jan-2026
+### G. Phillips
+
+###-----------------------------------------------------------------------
+### Start of script
+
+x<- 5
+print(x)
