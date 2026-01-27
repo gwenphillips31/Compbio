008be46aa07534a897408f274597f64dd982fb8d
Gwen Phillips <gwendolyn.phillips@uvm.edu>
2026-01-27 14:18:56 -0500

    Jan 27

--- r_scripts/atomicVecs.R
+++ r_scripts/atomicVecs.R
@@ -91,3 +91,122 @@ z+y
 ### recycling 
 x<- c(1,2)
 z+x
+
+
+##### Atomic vec pt II
+
+z<- vector(mode = "numeric", length=0)
+print (z)
+### dynamic creation (Dont in R, do in Python)
+z<- c(z,5)
+print (z)
+
+#### predefined length
+z<- rep(0,100)
+print (z)
+length (z)
+
+### empty vector
+z<- rep(NA, 100)
+print (z)
+
+typeof(z)
+
+z[1]<- "Vermont"
+head(z)
+typeof(z)
+
+my_vector <- runif(100)
+my_names <-paste("Species", seq(1:length(my_vector)),sep="") #length=100
+print (my_names)
+
+names(my_vector) <- my_names
+head(my_vector)
+str(my_vector)
+
+### Using the rep function
+rep(0.5, 6)
+rep(x=0.5, times= 6)
+my_vec <- c(1,2,3)
+
+
+### repeat a vector
+rep(x=my_vec, times=2, each=2)
+
+### seq vectors
+seq(from= 2, to= 4)
+2:4 # okay shorthand
+
+x<- seq(from= 2, to= 4, length= 7) ### evenly spaced with final length of 7
+
+my_vec <- 1:length (x) ### common in other languages, slow in R
+my_vec
+
+### Better in R
+seq_along(my_vec)
+
+### generating random vectors
+runif(5) #gives us 5 values from 0-1
+
+###The params
+set.seed(123) ### takes any number and gives you the same progression
+runif(n=1, min=0, max=1)
+
+### normal distribution
+out<- rnorm(n=500, mean =100, sd=30)
+hist(out)
+
+### random sampling
+
+long_vec<- seq_len(10)
+sample(x= long_vec, size=100, replace= T)
+
+### weighted sampling from a vec
+weights<- c(rep(20,5),rep(100,5))
+weights
+
+sample(x=long_vec, replace=T, prob=weights)
+
+### subsetting vectors
+z<- c(3.1, 9.2, 1.3, 0.4,7.5)
+print(z)
+
+z[-c(2,3)] #using vecs to slice
+z[z<3] # using logical
+
+# <   less than
+# >   greater than
+# <=  less than or equal to
+# >=  greater than or equal to
+# ==  equal to
+
+#logical vectors
+# ! not
+# & and (vector)
+# | or (vector)
+# xor(x,y)
+
+x<- 1:5
+y<- c(1:3, 7,7)
+
+x==2 # equals
+x !=2 #not equals
+x ==5 & y==7 ### using and
+x==1 | y== 7 ## or
+x==3 | y==3
+
+### missing values
+set.seed(90)
+z<- runif(10)
+z
+z<0.5
+z[z< 0.5]
+
+z[which(z<0.5)]
+
+zd<- c(z,NA, NA)
+
+zd[zd< 0.5]
+
+#### dropping NA with which to index
+zd[which(zd< 0.5)]
