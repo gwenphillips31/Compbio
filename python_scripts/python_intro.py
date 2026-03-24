41355be9cfda70c7ae5bb8b655535a547706eff4
Gwen Phillips <gwendolyn.phillips@uvm.edu>
2026-03-24 16:00:11 -0400

    class 3.24

--- python_scripts/python_intro.py
+++ python_scripts/python_intro.py
@@ -204,3 +204,187 @@ from numpy import random
 random.seed(seed=100)
 
 random.randint(50)
+
+x= random.normal(loc= 5, scale= 3, size= 200)
+plt.hist(x)
+plt.show(x)
+
+x= random.binomial(n = 10, p = 0.5, size= 300)
+
+plt.hist(x)
+plt.show
+
+x= random.uniform(low=1, high= 10, size =50)
+print(x)
+
+### math is still math
+
+x*100
+
+arr2*arr2
+
+np.mean(arr2)
+np.max(arr2)
+
+##############################################################
+### logic structures
+##############################################################
+### if.stat
+
+a=6
+if a >= 5:
+    print("a is greater than or eaqual to 5")
+
+if a >=6:
+    print ("a is greater than or equal to 5")
+else:
+    print("a is less than 5")
+
+a=3
+b=3
+operation= "mutl"
+
+if operation = "mult":
+    y=a*b
+elif operation == "div":
+    y= a/b
+elif operation == "add":
+    y= a+b
+elif operation == "sub":
+    y= a-b
+else:
+    y= "i dont know that operation"
+ 
+y
+
+#############################################################
+### Loops
+#############################################################
+
+l= [10, 20]
+
+for i in range(2):
+    print(l[i])
+
+### loop on an obj directly
+x= ["blue","green","red"]
+
+for i in x:
+    print(i)
+
+### a more complicated loop
+rnd= random.uniform(low=1, high= 5, size= 10)
+
+outList= [] ### truly empty list
+
+for i in range(len(arr1)):
+    outList.append(rnd[i]+ arr1[i])
+
+outList
+
+### nested loop with ifelse
+
+rnd2D= random.uniform(low= 0, high= 1, size= (3,3))
+rnd2D
+
+matOut = np.empty(shape= (3,3))
+shp = rnd2D.shape
+shp
+
+### nested loop
+for i in range(shp[0]):
+    for j in range(shp[1]):
+
+        if rnd2D[i,j] >p= 0.5:
+            matOut[i,j] = rnd2D[i,j]*1000
+        else:
+            matOut[i,j] = rnd2D[i,j]/1000
+
+matOut
+
+###############################################################################
+# PANDAS
+###############################################################################
+
+# ------ CREATING A DATA FRAME ------ #
+
+# make a date vec
+dates = pd.date_range("20130101", periods=6)
+
+# create a data frame with 4 numeric columns of length 6 named A, B, C, and, D
+df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list("ABCD"))
+df
+
+# ------ DF INFO ------ #
+
+df.head(4) # top 4 rows
+df.tail(4) # bottom 4 rows
+
+df.index # look at index variables
+df.columns # look at the columns
+df.describe() # summary stats for each row
+
+# convert to numpy array
+df.to_numpy()
+
+# ------ DF INDEXING ------ #
+
+# selecting rows and columns - BY LABELS
+df["A"] # column
+df.loc[:, ["A", "B"]] # columns
+df["20130102":"20130104"] # rows
+
+# slicing on both axises using the date labels
+df.loc["20130102":"20130104", ["A", "B"]]
+
+# selecting rows and columns - BY LOCATION
+df.iloc[3,2] # value at index 3,2
+df.iloc[[1, 2, 4], [0, 2]] # rows 1,2,4 and cols 0 and 2
+df.iloc[1:3, :] # First two rows all cols
+
+# ------ DF MANIPULATION ------ #
+
+# read in data sets
+ds = pd.read_csv("iris.data.csv")
+ds["sepal_length"] # pull sepal width out of data set
+
+ds.head() # examine the data set
+
+# adding a new columns
+ds["sepal_area"] = ds.sepal_length * ds.sepal_width
+ds["petal_area"] = ds["petal_length"] * ds["petal_width"]
+
+# boolean operations
+ds[ds["sepal_length"] > 7] #  all rows where sepal length is greater than 7
+
+# fully numeric data frame
+df[df > .5] # values in the data frame greater than 2 - everythin else is nan
+
+# ------ DF GROUPING AND SUMMARY ------ #           
+
+# mean of petal length and petal width for each species
+mean_table = ds.groupby("species")[["petal_length", "petal_width"]].mean()
+
+# let's make a long form data set and use hhierarchical indexing
+
+# make wide format to long
+ds_long = pd.melt(ds, id_vars=['species'], value_vars=["sepal_width", "sepal_length", "petal_width", "petal_length"],
+           var_name='vars', value_name='vals')
+
+
+# table with two indexes; species and variable
+mult_indx = ds_long.groupby(["species", "vars"]).mean()
+
+
+# pivot tables - another way of grouping
+pd.pivot_table(ds_long, values="vals", index=["vars"], columns=["species"], aggfunc=np.mean)
+pd.pivot_table(ds_long, values="vals", index=["vars"], columns=["species"], aggfunc=np.sum)
+
+
+# stacking:
+mean_table # original table
+mean_table.stack() # stacked table
+
+# stacking:
+mult_indx # original table
+mult_indx.unstack() # unstacked table
